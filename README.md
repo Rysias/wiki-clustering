6d80d5807690e952f68108cedf23c7b943e493f4
Rysias <jhvithamar@gmail.com>
2024-04-17 17:29:47 +0200

    add multi-wiki-clustering-p2p

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Code for creating clustering benchmarks for arbitrary languages using wikipedia
 
 ## TODO: 
 - [ ] Create a read-like file on HF a la [this one](https://huggingface.co/datasets/mteb/amazon_reviews_multi/blob/main/amazon_reviews_multi.py)
+- [ ] Simple documentation on how the data was created.
---
+++ src/multi-wiki-clustering-p2p.py
@@ -0,0 +1,88 @@
+import gzip
+import json
+from collections.abc import Generator
+
+import datasets
+
+_LANGUAGES = {
+    "da": "Danish",
+    "lv": "Latvian",
+    "sq": "Albanian",
+    "gv": "Manx",
+}
+_ALL_LANGUAGES = "all_languages"
+_DOWNLOAD_URL = "{lang}/{split}.jsonl.gz"
+_VERSION = "1.0.0"
+_DESCRIPTION = """
+A dataset of wikipedia paragraphs and corresponding top-level categories for multilingual clustering.
+"""
+
+
+class WikiClusteringP2PConfig(datasets.BuilderConfig):
+    """BuilderConfig for AmazonReviewsMultiConfig."""
+
+    def __init__(self, languages: dict[str, str] | None = None, **kwargs):  # noqa: ANN003
+        super().__init__(version=datasets.Version(_VERSION, ""), **kwargs)
+        self.languages = languages
+
+
+class WikiClusteringP2P(datasets.GeneratorBasedBuilder):
+
+    """The Multilingual Amazon Reviews Corpus"""
+
+    BUILDER_CONFIGS = [
+        WikiClusteringP2PConfig(
+            name=_ALL_LANGUAGES,
+            languages=_LANGUAGES,
+            description="A collection of Amazon reviews specifically designed to aid research in multilingual text classification.",
+        ),
+    ] + [
+        WikiClusteringP2PConfig(
+            name=lang,
+            languages=[lang],
+            description=f"{_LANGUAGES[lang]} examples from a collection of Amazon reviews specifically designed to aid research in multilingual text classification",
+        )
+        for lang in _LANGUAGES
+    ]
+    BUILDER_CONFIG_CLASS = WikiClusteringP2PConfig
+    DEFAULT_CONFIG_NAME = _ALL_LANGUAGES
+
+    def _info(self) -> datasets.DatasetInfo:
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            supervised_keys=None,
+        )
+
+    def _split_generators(
+        self,
+        dl_manager: datasets.DownloadManager,
+    ) -> list[datasets.SplitGenerator]:
+        test_urls = [
+            _DOWNLOAD_URL.format(split="test", lang=lang)
+            for lang in self.config.languages
+        ]
+
+        test_paths = dl_manager.download_and_extract(test_urls)
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"file_paths": []},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={"file_paths": []},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"file_paths": test_paths},
+            ),
+        ]
+
+    def _generate_examples(self, file_paths: list[str]) -> Generator[tuple[int, dict]]:
+        row_count = 0
+        for file_path in file_paths:
+            with gzip.open(file_path, "rt", encoding="utf-8") as f:
+                for line in f:
+                    yield row_count, json.loads(line)
+                    row_count += 1
